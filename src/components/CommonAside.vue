03f41ee3ecc2af86c77ccd7f2a46cad6393e04ec
gaoxizhong <296203386@qq.com>
2021-11-24 21:18:58 +0800

    恢复改

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -9,10 +9,10 @@
         default-active="0-0-0-0-0"
         unique-opened
     >
-      <div class="logo-box">
-        <img src="../assets/image/logo-2.png" alt="" v-show="!isCollapse" class="logo-img"/>
-        <h3 v-show="isCollapse">信泰</h3>
-      </div>
+    <div class="logo-box">
+      <img src="../assets/image/logo.png" alt="" v-show="!isCollapse" />
+      <h3 style="color:#fff;" v-show="isCollapse">菁苗</h3>
+    </div>
 
 			<!-- 第一层 -->
 			<template v-for="(item,index) in datalist">
@@ -141,11 +141,9 @@
   width: 100%;
   height: auto;
   padding: 10px 0;
-  background: #242a4d;
-  box-shadow: 0 2px 10px #000020;
 }
 img.logo-img{
-  width: 180px;
+  width: 220px;
 }
   .el-menu-vertical-demo:not(.el-menu--collapse) {
     width: 260px;
@@ -196,13 +194,13 @@ img.logo-img{
     display: -webkit-box;
     -webkit-line-clamp: 1;
     -webkit-box-orient: vertical;
-    color:#242a4d;
+    color:#fff;
     opacity: 0.7;
-    background: #fff !important;
+    background: #20234E !important;
 }
 .span-text{
-  color:#242a4d;
-  /* opacity: 0.8; */
+  color:#fff;
+  opacity: 0.7;
 }
 .el-submenu .el-menu-item.is-active{
   color: #fff;
@@ -210,10 +208,7 @@ img.logo-img{
   background: #8FA7FA !important;
 }
 .el-icon-arrow-down:before{
-  color: #242a4d;
-}
-.el-aside>ul>li:nth-of-type(1){
-  margin-top: 14px;
+  color: #fff;
 }
 </style>
 <script>
@@ -221,8 +216,8 @@ export default {
     props:['datalist'],
     data() {
         return {
-            menuBackgroundColor:'#fff',
-            menuTextColor:'#242a4d',
+            menuBackgroundColor:'#242a4d',
+            menuTextColor:'#fff',
             oneColumn: '0'
         }
     },
