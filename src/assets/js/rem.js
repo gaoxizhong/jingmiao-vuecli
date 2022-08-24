ce5f423b38e77ae12d99e57d10ae622e98ed5061
gaohaiyong <296203386@qq.com>
2022-08-25 00:43:42 +0800

    1

---
+++ src/assets/js/rem.js
@@ -0,0 +1,19 @@
+// 设置基准
+const BASE = 100;
+
+(function (doc, win) {
+  let docEl = doc.documentElement,
+ // orientationchange->手机屏幕转屏事件
+     // resize->页面大小改变事件
+      resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',//去windows遍历属性，如果有orientationchange这个属性就用他，没有就用resize
+      recall = function () {
+          let clientWidth = docEl.clientWidth;//获取到屏幕的宽度
+          console.log(clientWidth)
+          if (!clientWidth) return;
+          docEl.style.fontSize = BASE * (clientWidth / 1920) + 'px';
+      };
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recall, false);
+// DOMContentLoaded->dom加载完就执行,onload要dom/css/js都加载完才执行
+  doc.addEventListener('DOMContentLoaded', recall, false);
+})(document, window);
--- src/views/researchPages/researchHome.vue
+++ src/views/researchPages/researchHome.vue
@@ -1,3 +1,17 @@
 <template>
-  
+  <div class="aaa">1</div>
 </template>
+<script>
+  import '../../assets/js/rem'
+  export default {
+
+  }
+</script>
+<style scoped>
+  .aaa{
+    width: 1rem;
+    height: 3rem;
+    background: rgb(41, 160, 4);
+    font-size: 0.4rem;
+  }
+</style>
