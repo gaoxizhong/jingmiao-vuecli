cd3f76ce761493486b4081a8ec0bfc92e77029e4
gaohaiyong <296203386@qq.com>
2023-04-09 20:37:55 +0800

    测试环境

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,11 @@
 export default {
     title:'admin',
     baseURL: {
-        // 开发
-        dev: 'https://jm.jiankangche.cn/jmjk/',
-        // 生产
-        pro: 'https://jm.jiankangche.cn/jmjk/',
+        //主站
+        // dev: 'https://jm.jiankangche.cn/jmjk/',
+        //测试
+        dev: 'http://182.43.12.243:8981/jmjk/',
+        
         // webServices开发环境
         webServicesUrl:'/api',
         // webServices生产环境
