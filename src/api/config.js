e3818e48230e445e752b50edf73e097308e74c6e
gaoxizhong <296203386@qq.com>
2024-01-19 18:15:10 +0800

    优化

--- src/api/config.js
+++ src/api/config.js
@@ -1,12 +1,15 @@
+const httpUrl = window.location.host;
 export default {
     title:'admin',
     baseURL: {  // 主站
         // 正式环境
-        pro: 'http://182.43.12.243:8981/jmjk/'
+        pro: 'http://106.225.184.197:8981/jmjk/'
+        // pro: 'http://' + httpUrl +'/jmjk/',
     },
     researchURL: {  // 科研探索
         //测试环境
-        dev: 'http://182.43.12.243:8981/jmjk/',
+        dev: 'http://106.225.184.197:8981/jmjk/',
+    //    dev: 'http://' + httpUrl +'/jmjk/',
     },
     iframeURL: {  // 外部网站内嵌访问--科研探索
 
